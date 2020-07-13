eec833bd7aaeb38f483c5193b1420aada4ec6b5c
JIHATSUGOCHO <kcsfs2@gmail.com>
2020-07-13 12:26:20 +0900

    UCHI

--- app/src/main/java/com/example/maindisp/MyApp.kt
+++ app/src/main/java/com/example/maindisp/MyApp.kt
@@ -1,8 +1,7 @@
 package com.example.maindisp
 import android.app.Application
 class MyApp :Application(){
-
-
+    
     val QUESTION=Array<String?>(2400,{null})
     val ANSWER=Array<String?>(2400,{null})
     val NOTE=Array<String?>(20,{null})
