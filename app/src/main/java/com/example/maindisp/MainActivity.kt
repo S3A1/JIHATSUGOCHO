ffbcfaa531db151e3c0b97029d9b9b36096fb18b
kcsfs2@gmail.com <araorokuta2019>
2020-07-14 11:35:53 +0900

    Merge branch 'master' of https://github.com/S3A1/JIHATSUGOCHO
    # Conflicts:
    #	app/src/main/java/com/example/maindisp/MainActivity.kt
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/maindisp/MainActivity.kt
+++ app/src/main/java/com/example/maindisp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.maindisp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //insert test
+        list.setOnClickListener{tapDisp02(it)}
+
+    }
+    fun tapDisp02(view: View?){
+        val intent = Intent(this, Disp02::class.java)
+        startActivity(intent)
     }
 }
