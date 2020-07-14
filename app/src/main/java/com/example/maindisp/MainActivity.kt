7e130be6ff1d96ea3265075358aea587184b4e0e
JIHATSUGOCHO <kcsfs2@gmail.com>
2020-07-14 11:35:17 +0900

    UCHI

--- app/src/main/java/com/example/maindisp/MainActivity.kt
+++ app/src/main/java/com/example/maindisp/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.example.maindisp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+    val GLOBAL=MyApp.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //insert test
+    }
+
+    fun tap_btnWarpDisp07(view: View?){
+        GLOBAL.QUESTION[0]="Q1"
+        GLOBAL.ANSWER[0]="A1"
+        GLOBAL.QUESTION[1]="Q2"
+        GLOBAL.ANSWER[1]="A2"
+        val intent= Intent(this,Disp07::class.java)
+        startActivity(intent)
     }
 }
