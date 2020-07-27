93f157dbcaa9f000b3c2b0f4c5bfd151e47fc2fc
kcsfs2@gmail.com <araorokuta2019>
2020-07-27 13:56:41 +0900

    yoshida

--- app/src/main/java/com/example/maindisp/Disp02.kt
+++ app/src/main/java/com/example/maindisp/Disp02.kt
@@ -32,25 +32,34 @@ class Disp02 : AppCompatActivity() {
 
         val vg = findViewById<View>(R.id.tableLayout) as ViewGroup
         var i = 0
+        var num = 0
         while(GLOBAL.QUESTION[GLOBAL.NOTE_NUMBER*120+i] != null){
             getLayoutInflater().inflate(R.layout.table, vg)
-            val tr = vg.getChildAt(i) as TableRow
-            if(i==0){
+            if(num==0){
+                val tr = vg.getChildAt(i) as TableRow
                 ((tr.getChildAt(1))as Button).setText("テストモード")
                 ((tr.getChildAt(1)) as Button).setOnClickListener {
-
                 }
+                num++
+            }else{
+                val tr = vg.getChildAt(num) as TableRow
+                ((tr.getChildAt(0))as CheckBox).isChecked()
+                ((tr.getChildAt(0))as CheckBox).setTag(i)
+                ((tr.getChildAt(1)) as Button).setTag(i)
+                ((tr.getChildAt(1)) as Button).setText(GLOBAL.QUESTION[GLOBAL.NOTE_NUMBER*120+i])
+                ((tr.getChildAt(1)) as Button).setOnClickListener {
+                    //GLOBAL.NOTE_NUMBER=Integer.parseInt(it.getTag().toString())
+                    //GLOBAL.PAGE_NUMBER=0
+                    //tap_btnWarpDisp02(it)
+                    GLOBAL.NOTE_NUMBER
+                    GLOBAL.PAGE_NUMBER=i
+                    val intent= Intent(this,Disp07::class.java)
+                    startActivity(intent)
+                }
+                num++
+                i++
             }
-            ((tr.getChildAt(0))as CheckBox).isChecked()
-            ((tr.getChildAt(0))as CheckBox).setTag(i)
-            ((tr.getChildAt(1)) as Button).setTag(i)
-            ((tr.getChildAt(1)) as Button).setText(GLOBAL.QUESTION[GLOBAL.NOTE_NUMBER*120+i])
-            ((tr.getChildAt(1)) as Button).setOnClickListener {
-                //GLOBAL.NOTE_NUMBER=Integer.parseInt(it.getTag().toString())
-                //GLOBAL.PAGE_NUMBER=0
-                //tap_btnWarpDisp02(it)
-            }
-            i++
+
         }
 
 
