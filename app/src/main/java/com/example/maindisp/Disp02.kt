4dda4454cd3290a2ce2c896f659da60395407342
JIHATSUGOCHO <kcsfs2@gmail.com>
2020-07-27 10:59:47 +0900

    UCHI

--- app/src/main/java/com/example/maindisp/Disp02.kt
+++ app/src/main/java/com/example/maindisp/Disp02.kt
@@ -18,7 +18,7 @@ class Disp02 : AppCompatActivity() {
         setContentView(R.layout.activity_disp02)
         setSupportActionBar(toolbar)
 
-        btnWarpDisp02.text = GLOBAL.NOTE[GLOBAL.NOTE_NUMBER]+" "+GLOBAL.NOTE_NUMBER.toString()
+        btnWarpDisp02.text = GLOBAL.NOTE[GLOBAL.NOTE_NUMBER]
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
