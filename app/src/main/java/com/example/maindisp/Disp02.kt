29f76a16435f9d0e0c0cb36c288d01ff0d7f1ce5
JIHATSUGOCHO <kcsfs2@gmail.com>
2020-09-01 12:10:13 +0900

    UCHI

--- app/src/main/java/com/example/maindisp/Disp02.kt
+++ app/src/main/java/com/example/maindisp/Disp02.kt
@@ -23,7 +23,6 @@ class Disp02 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_disp02)
-        setSupportActionBar(bar)
         CreatePage()
     }
 
