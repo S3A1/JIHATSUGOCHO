5116cfc91ff8fe16da743dc4b22855a385507a10
JIHATSUGOCHO <kcsfs2@gmail.com>
2020-09-11 12:03:11 +0900

    UCHI

--- app/src/main/java/com/example/maindisp/MyApp.kt
+++ app/src/main/java/com/example/maindisp/MyApp.kt
@@ -9,7 +9,7 @@ class MyApp :Application(){
     var QUESTION=Array<String?>(2400,{null})//問題文
     var ANSWER=Array<String?>(2400,{null})//回答文
     var LAST=Array<Int?>(2400,{null})//前回正誤
-    var NOTE=Array<String?>(20,{null})//ノートのタイトル
+    var NOTE=Array<String?>(50,{null})//ノートのタイトル
     var PAGE_NUMBER:Int=0//ページ番号管理用 0-119
     var NOTE_NUMBER:Int=0//ノート番号管理用 0-19
     var TIMESPAN:Int=5//通知間隔設定用
